073664c6b875efd5de940089a5398f62d8975666
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-03-17 23:23:07 +0500

    feat: add router

--- my-components/src/App.tsx
+++ my-components/src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './App.css';
 
-import SearchBar from './components/SearchBar/SearchBar';
 import Main from './pages/Main/Main';
 import Header from './components/Header/Header';
+import { NavigateFunction, Route, Routes, useNavigate } from 'react-router-dom';
 
-class App extends React.Component {
+class App extends React.Component<{ navigate: NavigateFunction; }> {
   render() {
     return(
       <div>
-        <Header />
-        <SearchBar />
-        <Main />
+        <Header navigate={this.props.navigate} />
+        <Routes>
+          <Route path='home' element={<Main />}></Route>
+        </Routes>
       </div>
     );
   }
+}
+
+export function AppWithRouter() {
+  const navigate = useNavigate();
+  return(<App navigate={navigate}></App>)
 }
 
 export default App;
--- my-components/src/components/Header/Header.tsx
+++ my-components/src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link, NavigateFunction } from "react-router-dom";
 
-class Header extends React.Component {
+class Header extends React.Component<{ navigate: NavigateFunction; }> {
     render(): React.ReactNode {
         return(
             <header>
-                
+                <Link to="home">Home</Link>
+                <Link to="about">About</Link>
             </header>
         );
     }
