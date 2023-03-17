6e7ee07f61225c767528c81a6af8d4a2cb7c223e
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-03-17 22:38:15 +0500

    feat: add header component

--- my-components/src/App.tsx
+++ my-components/src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 
 import SearchBar from './components/SearchBar/SearchBar';
 import Main from './pages/Main/Main';
+import Header from './components/Header/Header';
 
 class App extends React.Component {
   render() {
     return(
       <div>
+        <Header />
         <SearchBar />
         <Main />
       </div>
