9eda495e12288f4320d5b372be6802e4347e1a20
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-03-17 17:55:25 +0500

    feat: add search input

--- my-components/src/App.tsx
+++ my-components/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import SearchBar from './components/SearchBar/SearchBar';
+
 class App extends React.Component {
   render() {
     return(
-      <div>Hello</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
---
+++ my-components/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    state = {
+        inputVal: localStorage.getItem('inputVal') || ''
+    }
+
+    handleInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        console.log(e.currentTarget.value);
+        this.setState({inputVal: e.currentTarget.value});
+        localStorage.setItem('inputVal', e.currentTarget.value);
+    }
+
+
+    render(): React.ReactNode {
+        const {inputVal} = this.state;
+        return(
+            <div className="search-bar">
+                <div className="search-bar-container">
+                    <input 
+                        onInput={this.handleInput}
+                        value={inputVal}
+                        className="search-input"
+                        type="text"
+                        placeholder="Search..."
+                    />
+                    <button type="submit" className="btn">Search</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
