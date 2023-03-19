9f1293a8629436b4db32597329071e306c5687bf
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-03-19 12:01:38 +0500

    feat: add about page

--- my-components/src/App.tsx
+++ my-components/src/App.tsx
@@ -5,6 +5,7 @@ import Main from './pages/Main/Main';
 import Header from './components/Header/Header';
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import NotFound from './pages/NotFound/NotFound';
+import About from './pages/About/About';
 
 class App extends React.Component<any> {
   render() {
@@ -13,6 +14,7 @@ class App extends React.Component<any> {
         <Header navigate={this.props.navigate} location={this.props.location} />
         <Routes>
           <Route path='home' element={<Main />}></Route>
+          <Route path='about' element={<About />}></Route>
           <Route path='*' element={<NotFound />}></Route>
         </Routes>
       </div>
