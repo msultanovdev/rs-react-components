67e2007e668c61e7f544ebb5e484330e03299eb1
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-03-27 14:50:26 +0500

    feat: add select & checkbox

--- my-components/src/pages/FormPage/FormPage.tsx
+++ my-components/src/pages/FormPage/FormPage.tsx
@@ -2,23 +2,24 @@ import React from "react";
 import "./FormPage.css";
 
 class FormPage extends React.Component {
-  componentDidMount(): void {
-    alert(
-      "Dear Reviewer, please give time to finish this task, check it at the last day of the cross-check))"
-    );
-  }
   state = {
     isValidName: false,
     isValidDate: false,
+    isValidCar: false,
+    isAgree: false,
   };
 
   nameInputRef: React.RefObject<HTMLInputElement>;
   birthdayRef: React.RefObject<HTMLInputElement>;
+  favCarRef: React.RefObject<HTMLSelectElement>;
+  checkAgreeRef: React.RefObject<HTMLInputElement>;
 
   constructor(props: never) {
     super(props);
     this.nameInputRef = React.createRef<HTMLInputElement>();
     this.birthdayRef = React.createRef<HTMLInputElement>();
+    this.favCarRef = React.createRef<HTMLSelectElement>();
+    this.checkAgreeRef = React.createRef<HTMLInputElement>();
   }
 
   submitButton(e: React.MouseEvent<HTMLButtonElement>) {
@@ -28,10 +29,23 @@ class FormPage extends React.Component {
     } else {
       this.setState({ isValidName: false });
     }
+
     if (!(this.birthdayRef.current as HTMLInputElement).value) {
       this.setState({ isValidDate: true });
     } else {
       this.setState({ isValidDate: false });
+    }
+
+    if (!(this.favCarRef.current as HTMLSelectElement).value) {
+      this.setState({ isValidCar: true });
+    } else {
+      this.setState({ isValidCar: false });
+    }
+
+    if (!(this.checkAgreeRef.current as HTMLInputElement).checked) {
+      this.setState({ isAgree: true });
+    } else {
+      this.setState({ isAgree: false });
     }
   }
 
@@ -66,6 +80,32 @@ class FormPage extends React.Component {
                 : ""}
             </p>
           </div>
+
+          <div className="input-wrapper">
+            <div className="select-wrapper">
+              <p>Your favourite car mark: </p>
+              <select defaultValue="Merc" ref={this.favCarRef}>
+                <option value="Merc">Mercedes</option>
+                <option value="BMW">BMW</option>
+                <option value="Ferrari">Ferrari</option>
+                <option value="Porche">Porche</option>
+              </select>
+            </div>
+            <p className="input-invalid">
+              {this.state.isValidCar ? "Please, choose one car!" : ""}
+            </p>
+          </div>
+
+          <div className="input-wrapper">
+            <div className="select-wrapper">
+              <p>Do you agree?: </p>
+              <input type="checkbox" ref={this.checkAgreeRef} />
+            </div>
+            <p className="input-invalid">
+              {this.state.isAgree ? "Check the checkbox" : ""}
+            </p>
+          </div>
+
           <button
             onClick={(e) => this.submitButton(e)}
             type="submit"
