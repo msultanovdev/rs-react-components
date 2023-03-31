a4c48102eb604687e8ef8dec4797c40ff50d1760
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-03-31 17:52:20 +0500

    feat: migrate notfound to fn component

--- my-components/src/pages/NotFound/NotFound.tsx
+++ my-components/src/pages/NotFound/NotFound.tsx
@@ -1,14 +1,11 @@
-import React from "react";
 import "./NotFound.css";
 
-class NotFound extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <div data-testid="not-found" className="notfound">
-        <h1>Not Found Page (404 Error)</h1>
-      </div>
-    );
-  }
-}
+const NotFound = () => {
+  return (
+    <div data-testid="not-found" className="notfound">
+      <h1>Not Found Page (404 Error)</h1>
+    </div>
+  );
+};
 
 export default NotFound;
