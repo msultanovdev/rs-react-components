3f3108437414aaa600c0153dcf01b38c2328f3b9
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-03-31 16:19:00 +0500

    feat: migrate app component to fn component

--- my-components/src/App.tsx
+++ my-components/src/App.tsx
@@ -3,40 +3,23 @@ import "./App.css";
 
 import Main from "./pages/Main/Main";
 import Header from "./components/Header/Header";
-import {
-  NavigateFunction,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NotFound from "./pages/NotFound/NotFound";
 import About from "./pages/About/About";
 import FormPage from "./pages/FormPage/FormPage";
 
-class App extends React.Component<{
-  navigate: NavigateFunction;
-  location: string;
-}> {
-  render() {
-    return (
-      <div>
-        <Header navigate={this.props.navigate} location={this.props.location} />
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="about" element={<About />}></Route>
-          <Route path="form-page" element={<FormPage />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-      </div>
-    );
-  }
-}
-
-export function AppWithRouter() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  return <App navigate={navigate} location={location.pathname} />;
-}
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="form-page" element={<FormPage />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
--- my-components/src/components/Header/Header.tsx
+++ my-components/src/components/Header/Header.tsx
@@ -1,31 +1,25 @@
 import React from "react";
-import { Link, NavigateFunction } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Header.css";
 
-class Header extends React.Component<{
-  navigate: NavigateFunction;
-  location: string;
-}> {
-  render(): React.ReactNode {
-    return (
-      <header className="header">
-        <nav>
-          <Link className="nav-link" to="/">
-            Home
-          </Link>
-          <Link className="nav-link" to="about">
-            About
-          </Link>
-          <Link className="nav-link" to="form-page">
-            Form
-          </Link>
-        </nav>
-        <p className="header-path">
-          {this.props.location.slice(1, this.props.location.length)}
-        </p>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  const location = useLocation().pathname;
+  return (
+    <header className="header">
+      <nav>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="about">
+          About
+        </Link>
+        <Link className="nav-link" to="form-page">
+          Form
+        </Link>
+      </nav>
+      <p className="header-path">{location.slice(1, location.length)}</p>
+    </header>
+  );
+};
 
 export default Header;
