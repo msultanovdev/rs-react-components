bec852d6aecc331358c303540f5ecbf26ca49367
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-03-31 16:27:59 +0500

    feat: migrate form-card to fn component

--- my-components/src/components/FormCard/FormCard.tsx
+++ my-components/src/components/FormCard/FormCard.tsx
@@ -1,21 +1,18 @@
-import React from "react";
 import "./FormCard.css";
 import { cardProps } from "../../types";
 
-class FormCard extends React.Component<cardProps> {
-  render(): React.ReactNode {
-    return (
-      <div className="form-card">
-        <div>
-          <p>Name: {this.props.name}</p>
-          <p>Birthday: {this.props.date}</p>
-          <p>Sex: {this.props.chooseSelection}</p>
-          <p>Favourite Car: {this.props.car}</p>
-          <p>File path: {this.props.file}</p>
-        </div>
+const FormCard = ({ name, date, chooseSelection, car, file }: cardProps) => {
+  return (
+    <div className="form-card">
+      <div>
+        <p>Name: {name}</p>
+        <p>Birthday: {date}</p>
+        <p>Sex: {chooseSelection}</p>
+        <p>Favourite Car: {car}</p>
+        <p>File path: {file}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default FormCard;
