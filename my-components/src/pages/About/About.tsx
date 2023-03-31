7674e7753faf8a684dcc844e9944347e82179a50
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-03-31 17:53:10 +0500

    feat: migrate about page to fn components

--- my-components/src/pages/About/About.tsx
+++ my-components/src/pages/About/About.tsx
@@ -1,14 +1,11 @@
-import React from "react";
 import "./About.css";
 
-class About extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <div data-testid="about" className="about">
-        <h1 data-testid="about-title">About Page</h1>
-      </div>
-    );
-  }
-}
+const About = () => {
+  return (
+    <div data-testid="about" className="about">
+      <h1 data-testid="about-title">About Page</h1>
+    </div>
+  );
+};
 
 export default About;
